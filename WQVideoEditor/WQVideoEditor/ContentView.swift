c898693cf445d010548d68c7b804d41a36b66356
zouying <13229309282@2980.com>
2020-09-14 21:51:26 +0800

    init project

---
+++ WQVideoEditor/WQVideoEditor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WQVideoEditor
+//
+//  Created by xunni zou on 2020/9/14.
+//  Copyright © 2020 xunni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
